f4e644da8ef3f5d7362a468f99692a20556f39d3
zxSHOXxz <lalameatlala123@gmail.com>
2024-10-04 15:14:54 +0300

    corss

--- app/Http/Middleware/CheckForMaintenanceMode.php
+++ app/Http/Middleware/CheckForMaintenanceMode.php
@@ -16,7 +16,7 @@ class CheckForMaintenanceMode
     public function handle($request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', 'https://amralkhozondar.com') // maybe put this into the .env file so you can change the URL in production.
+            ->header('Access-Control-Allow-Origin', 'http://127.0.0.1:8000') // maybe put this into the .env file so you can change the URL in production.
             ->header('Access-Control-Allow-Methods', '*') // or specify `'GET, POST, PUT, DELETE'` etc as the second parameter if you want to restrict the methods that are allowed.
             ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization'); // or add your headers.
     }
